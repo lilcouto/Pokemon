088771a2afc5f59ec61d8bd137e59621ec81fcf9
Patricia Hokama <patricia.hokama12@gmail.com>
2023-10-25 10:26:12 -0300

    Testes na página planta - consumindo API

--- assets/js/planta.js
+++ assets/js/planta.js
@@ -6,36 +6,46 @@ const createCardComponent = () => {
     const imgbox = document.createElement("div");
     imgbox.classList.add("imgbox");
     pokemonlinha.appendChild(imgbox);
-    const img = document.createElement("img");
-    img.src = 
-    imgbox.appendChild(img);
+    if (pokemon.types.type.name == "grass") {
+        fetch('https://pokeapi.co/api/v2/pokemon' + PokemonName)
+            .then(response => response.json())
+            .then(response => {
+            createPokemonImage(response.sprites.front_default);
+        });
+    }
     const info = document.createElement("div");
     pokemonlinha.appendChild(info);
     const numero = document.createElement("p");
     numero.classList.add("numero");
+    if (pokemon.types.type.name == "grass") {
+        numero.innerText = "#" + pokemon.order};
+    info.appendChild(nome);
     info.appendChild(numero);
     const nome = document.createElement("p");
     nome.classList.add("nome");
+    if (pokemon.types.type.name == "grass") {
+        nome.innerText = pokemon.name};
     info.appendChild(nome);
-    /*nome.innerText = PokemonName;*/
 
     return pokemonlinha;
 }
 
-/*function createPokemonImage(url) {
+function createPokemonImage(url) {
+    const img = document.createElement("img");
     img.src = url;
-    img.width = 100; 
+    img.width = 100;
+    document.getElementsByClassName("imgbox").append(img); 
 }
 
 
-fetch('https://pokeapi.co/api/v2/type/grass/')
+fetch('https://pokeapi.co/api/v2/pokemon/?limit=70')
     .then(response => response.json())
-    .then(response => console.log(createCardComponent()));
-        /*response.results.forEach(nome) => {
-        createCardComponent(nome)
-    }*/
-    /*(response.pokemon[0].name)*/
+    .then(response => {
+        response.results.name.forEach((nome) => {
+            console.log(createCardComponent(response.pokemon[0].pokemon.name))
+        })});
+        
 
-fetch('https://pokeapi.co/api/v2/type/grass/')
+/*fetch('https://pokeapi.co/api/v2/pokemon/?limit=70')
     .then(response => response.json())
-    .then(response => console.log(createCardComponent()));    
+    .then(response => console.log(createCardComponent()));*/
